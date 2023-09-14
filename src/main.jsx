1cf00b7326282f5b428f09530771a0f5550d37dd
Federico galan <federico@dev.fibertel.com.ar>
2023-09-14 00:48:50 -0300

    Se agregan assets al proyecto

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { Contacto } from './Routes/Contacto'
-import { Details } from './Routes/Details'
-import { Favs } from './Routes/Favs'
-import { Home } from './Routes/Home'
+import { Contacto } from './routes/Contacto'
+import { Details } from './routes/Details'
+import { Favs } from './routes/Favs'
+import { Home } from './routes/Home'
 import './main.css'
-// import { DoctorsProvider } from './context/DoctorsContext'
+import { Notfound } from './routes/Notfound'
+import { App } from './App'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <DoctorsProvider> */}
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/contacto' element={<Contacto/>}/>
-          <Route path='/details' element={<Details/>}/>
-          <Route path='/favourites' element={<Favs/>}/>
+          <Route path='/' element={<App/>}>
+            <Route path='/home' element={<Home/>}/>
+            <Route path='/contact' element={<Contacto/>}/>
+            <Route path='/details' element={<Details/>}/>
+            <Route path='/favourites' element={<Favs/>}/>
+            <Route path="*" element={<Notfound/>}/>
+          </Route>
         </Routes>
       </BrowserRouter>
-    {/* </DoctorsProvider> */}
   </React.StrictMode>,
 )
