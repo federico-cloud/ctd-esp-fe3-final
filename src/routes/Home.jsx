29baf765babca28bc3bda21b889bb1eaec398dab
Federico galan <federico@dev.fibertel.com.ar>
2023-09-14 00:50:15 -0300

    Se agrega contexto al Home

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,9 +1,12 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import Card from "../components/Card";
+import { ThemeContext } from "../context/ThemeContext";
 import '../css/Home.css'
 
 export const Home = () => {
     
+  const { theme } = useContext(ThemeContext);
+
   const getDoctors = async () => {
     const URL = 'https://jsonplaceholder.typicode.com/users';
     const resp = await fetch(URL);
@@ -25,20 +28,21 @@ export const Home = () => {
   }, []);
 
   return (
-    <>
+    <div className={`${theme}`}>
       <h2>HOME</h2>
       <div className='doctorsContainer'>
         {doctors.map(({id, name, address }) => {
           return ( 
-              <Card 
-                key={id} 
-                id={id} 
-                name={name} 
-                city={address.city} 
-              />
-            )})}
+            <Card 
+              key={id} 
+              id={id} 
+              name={name} 
+              city={address.city} 
+            />
+          )
+        })}
       </div>
-    </>
+    </div>
   )
 }
 
