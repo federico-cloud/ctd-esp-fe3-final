8b8b70c8296f3f45dd1679eab388e081e2b6d185
Federico galan <federico@dev.fibertel.com.ar>
2023-09-14 00:51:57 -0300

    Se agrega el provider del ThemeContext a la app

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { Outlet } from "react-router-dom"
 import { Navbar } from "./components/Navbar"
 import { Footer } from "./components/Footer"
-import './App.css'
+import { ThemeProvider } from "./context/ThemeContext"
 
 export const App = () => {
   return (
-    <>
+    <ThemeProvider>
       <Navbar/>
       <Outlet/>
       <Footer/>
-    </>
+    </ThemeProvider>
   )
 }
