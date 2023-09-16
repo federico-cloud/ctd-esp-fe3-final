c46057b0967293d6170fb6839e52fc3f8bf058b3
Federico galan <federico@dev.fibertel.com.ar>
2023-09-16 11:44:13 -0300

    hasta aca llegue con el desarrollo, falto arreglar el bug por el cual no se desmontan las card al sacarle el like

--- src/routes/Favs.jsx
+++ src/routes/Favs.jsx
@@ -1,7 +1,31 @@
+import { useContext, useEffect } from "react"
 import "../css/Favs.css"
+import Card from "../components/Card"
+import { ThemeContext } from "../context/ThemeContext"
 
 export const Favs = () => {
+
+  const {theme} = useContext(ThemeContext)
+
+  const doctorsFav = JSON.parse(localStorage.getItem("favs"))
+
+  useEffect(() => {
+    
+
+  }) 
+
   return (
-    <div className="favs">Favs</div>
+    <>
+      <div className={`favs ${theme}`}>
+        {doctorsFav.map(({city, id, name}) => (
+          <Card
+            key={id}
+            id={id}
+            name={name}
+            city={city}
+          />
+        ))}
+      </div>
+    </>
   )
 }
