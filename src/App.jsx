217577eb316a968181b7e6e56d9719f8c5755b2e
Federico galan <federico@dev.fibertel.com.ar>
2023-09-16 11:01:43 -0300

    Se agrega el contexto para obtener los datos de la API

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import { Outlet } from "react-router-dom"
 import { Navbar } from "./components/Navbar"
 import { Footer } from "./components/Footer"
 import { ThemeProvider } from "./context/ThemeContext"
+import { DataProvider } from "./context/DataContext"
 
 export const App = () => {
   return (
     <ThemeProvider>
+      <DataProvider>
       <Navbar/>
       <Outlet/>
       <Footer/>
+      </DataProvider>
     </ThemeProvider>
   )
 }
