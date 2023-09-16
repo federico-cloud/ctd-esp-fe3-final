fcaa18f8e5bc1c622c102553099ed043feebe7f5
Federico galan <federico@dev.fibertel.com.ar>
2023-09-16 11:37:28 -0300

    Se finaliza el componente para las rutas no encontradas

--- src/routes/Notfound.jsx
+++ src/routes/Notfound.jsx
@@ -1,5 +1,16 @@
+import { useContext } from 'react'
+import { ThemeContext } from '../context/ThemeContext'
+import '../css/Notfound.css'
+
 export const Notfound = () => {
+
+  // Damos contexto del tema
+  const { theme } = useContext(ThemeContext)
+
   return (
-    <div>ERROR</div>
+    <div className={`notfound ${theme}`}>
+      <img src="src/assets/error.gif" alt="Gif de error"/>
+      <p>ERROR: The page doesn't exists</p>
+    </div>
   )
 }
