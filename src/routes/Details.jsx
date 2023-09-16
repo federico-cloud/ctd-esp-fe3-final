09858418c090c1cc8a96278cf825c91d0d0b310f
Federico galan <federico@dev.fibertel.com.ar>
2023-09-16 11:05:34 -0300

    Se agrega la ruta para mostrar los detalles del doctor

--- src/routes/Details.jsx
+++ src/routes/Details.jsx
@@ -1,11 +1,33 @@
-import { useContext } from "react"
+import { useContext, useEffect, useState } from "react"
 import { ThemeContext } from "../context/ThemeContext"
+import "../css/Details.css"
+import { DataContext } from "../context/DataContext"
 
 export const Details = () => {
 
   const { theme } = useContext(ThemeContext)
+  const { data } = useContext(DataContext)
+  const [doctor, setDoctor] = useState({})
+  const idUrl = parseInt(location.pathname.split("/").pop())
+  
+  useEffect(() => {
+    data.map(doctor =>{
+      if (doctor.id === idUrl) {
+        setDoctor(doctor)
+      }
+    })
+  },[data])
 
   return (
-    <div className={`${theme}`}>Details</div>
+    <div className={`details ${theme}`}>
+      <h2>User Info</h2>
+      <div className="card-details">
+        <p><strong>ID:</strong>{doctor.id}</p>
+        <p><strong>Name:</strong>{doctor.name}</p>
+        <p><strong>Email:</strong>{doctor.email}</p>
+        <p><strong>Phone:</strong>{doctor.phone}</p>
+        <p><strong>Web site:</strong>{doctor.website}</p>
+      </div>
+    </div>
   )
 }
